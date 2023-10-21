86ff54e70e665cfa361ce63b55b13bfdf8f04376
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2023-10-21 17:24:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+// Introduction
+// console.log("Bhad me jao")
+
+
+//Variables
+// Let var const 
+// var bhumi="Rasgulla";
+// console.log(bhumi)
+// var bhumi="Chaat";
+// {
+//     var bhumi="Chole Bhature"
+// }
+// console.log(bhumi)
+// let aniket="Chaos"
+// aniket="Calm"
+// console.log(aniket)
+// aniket="Super Chaos"
+// const kritika="Gossips"
+// console.log(kritika)
+// kritika="Haryana"
+
+//Datatypes
+// let string1="Gouransh"
+// console.log(typeof string1)
+// let num=128
+// console.log(typeof num )
+
+// let bool=true
+// console.log(typeof bool)
+
+// let nalla=null
+// console.log(typeof nalla )
+
+// let und;
+// console.log(typeof  und)
+
+// let bint=6516516565165165156n;
+// console.log(typeof bint)
+// let Anuj=["coding","AlwaysSerious","Girls"]
+// console.log(Anuj)
+// let devdiya={
+//     hobbies:"Starring",
+//     Watching:"........",
+//     sleeping:"with ...."
+// }
+// console.log(devdiya)
+// function Ruhani(){
+//     console.log("Maintain Profe.....")
+// }
+// Ruhani()
+
+
+
+
+
+//Typecasting
+//https://www.w3schools.com/js/js_type_conversion.asp
+/*
+str-num
+num-str
+bool-num
+num-bool
+*/
+// var typeCast1="aigiy"
+// console.log(typeof typeCast1)
+// typeCast1=Number(typeCast1)
+// console.log(typeof typeCast1)
+//let bool=0
+// console.log(typeof bool)
+// bool1=Boolean(bool)
+// console.log(typeof bool1)
+
+
+//Operators
+/* += -= *= /= **=  */
+// let num1=35
+// console.log(num1)
+// num1*=35
+// console.log(num1)
+
+/* == != === !==*/ 
+// let p=55
+// let l="55"
+// console.log(l==p)
+/* && || !*/ 
+
+
+
+
+
+
+//Strings
+//https//developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// let p="Cute"
+// let n="iya"
+// console.log(`${p}${n}`)
+// console.log(p+n)
+// let str1="             Janviiiiiiiii              "
+// const str2= new String("Janviiiiiiiiiiiiiiiii")
+// console.log(str1)
+// console.log(str2)
+// console.log(str2.length)
+// console.log(str2.charAt("n"))
+// console.log(str1.trim())
+// console.log(str1)
+
+
+
+
+//Functions
+// function saksham(){
+//     console.log("I am AHOLE")
+// }
+// saksham()
+// let papa=()=>{
+//     return "Macbook"
+// };
+// let pop=papa();
+// console.log(pop);
+// (function hahahah() {
+//     console.log("Jinga LAla HUHUH")
+//   })();
+  
+
+
+
+
+//Conditnals
+//  var lal= "9/11"
+// if (Osama=="Peacemaker"){
+//     console.log("Allah hu Akbar")
+// }
+// else if(Osama=="Terrorist"){
+//     console.log("Katle aam karo iska")
+// }
+// else{
+//     console.log("insahh alllah maksad mat bhoolna Bhaijaan")
+// }
+// let date="9/11" ? console.log("Bomb It"): console.log("PrepTime boys the date is coming")
+
+// let nayan="Mental Gymnastics"
+// switch(nayan){
+//     case "Mental Gymnatics":
+//         console.log("HeHeHuHa")
+//         break
+//     default:
+//         console.log("Lululu")
+// }
+// switch(nayan){
+//     case:
+//     break
+
+
+//     default:
+
+// }
+
+
+
+
